fd761489fd1e9502469bdcd26662083c2b282136
Jon Fink <jon.fink@gmail.com>
2010-05-05 18:57:39 -0400

    Caught bug in kernel function!

--- src/KernelFunction.cc
+++ src/KernelFunction.cc
@@ -15,15 +15,15 @@ SqExpKernel::SqExpKernel(const Col<REAL>& param)
 REAL SqExpKernel::Eval(const Col<REAL>& a, const Col<REAL> &b)
 {
   REAL dist = norm(a-b, 2);
-  return param(0)*exp(-1/(2*param(1)*param(1))*dist);
+  return param(0)*exp(-(dist*dist)/(2*param(1)*param(1)));
 }
 
 void SqExpKernel::Grad(Col<REAL> &grad, const Col<REAL>& a, const Col<REAL>& b)
 {
   REAL dist = norm(a-b, 2);
   grad.set_size(param.n_elem);
-  grad(0) = exp(-dist/(2*param(1)*param(1)));
-  grad(1) = dist*param(0)*(1/pow(param(1),3))*exp(-dist/(2*param(1)*param(1)));
+  grad(0) = exp(-(dist*dist)/(2*param(1)*param(1)));
+  grad(1) = dist*dist*param(0)*(1/pow(param(1),3))*exp(-(dist*dist)/(2*param(1)*param(1)));
 }
 
 void SqExpKernel::GradX(Col<REAL> &grad, const Col<REAL>& a, const Col<REAL>& b)
@@ -35,9 +35,9 @@ void SqExpKernel::GradX(Col<REAL> &grad, const Col<REAL>& a, const Col<REAL>& b)
     grad.fill(0);
   }
   else {
-    grad(0) = -((a(0) - b(0))*param(0))*exp(-dist/(2*param(1)*param(1)))/
+    grad(0) = -((a(0) - b(0))*param(0))*exp(-(dist*dist)/(2*param(1)*param(1)))/
       (2*param(1)*param(1)*dist);
-    grad(1) = -((a(1) - b(1))*param(0))*exp(-dist/(2*param(1)*param(1)))/
+    grad(1) = -((a(1) - b(1))*param(0))*exp(-(dist*dist)/(2*param(1)*param(1)))/
       (2*param(1)*param(1)*dist);
   }
 }
